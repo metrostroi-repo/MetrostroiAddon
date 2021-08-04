4fbeff31b1deec827e5b24fdd629aa0b70639527
alexandr_sizov <aleksandr.sizov@outlook.com>
2021-08-04 22:13:18 +0300

    Fix controller logic

--- lua/entities/gmod_track_signal/init.lua
+++ lua/entities/gmod_track_signal/init.lua
@@ -644,12 +644,12 @@ function ENT:Think()
 		end
 	else
 		local number = self.RouteNumberReplace or ""
-		self.PrevTime = self.PrevTime or 0
+		--[[self.PrevTime = self.PrevTime or 0
 		if (CurTime() - self.PrevTime) > 1.0 then
 			self.PrevTime = CurTime()+math.random(0.5,1.5)
 			self:ARSLogic(self.PrevTime - CurTime())
 			self:CheckOccupation()
-		end
+		end]]
 		--[[
 		if self.MU or self.ARSOnly or self.RouteNumberSetup and self.RouteNumberSetup ~= "" or self.RouteNumber and self.RouteNumber ~= "" then
 			if self.NextSignalLink then
