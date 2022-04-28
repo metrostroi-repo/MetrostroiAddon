207b192e35560d61a936c282d650f1f1c8287ba5
Moon Horse <akaikangaroo@gmail.com>
2022-04-29 01:01:26 +0300

    Revert "Update init.lua"
    This reverts commit f3658902b2d8774a31b6c61563b1c8e67b43caea.

--- lua/entities/gmod_subway_81-717_mvm/init.lua
+++ lua/entities/gmod_subway_81-717_mvm/init.lua
@@ -563,7 +563,7 @@ function ENT:Think()
                         math.min(1,Panel.Headlights2)*0.40
     --local T = {}
 
-    self:SetPackedBool("ShowDVR",self.A75.Value)
+    self:SetPackedBool("ShowDVR",self.A75.Value)				--Потому что в головных вагонах не смоделили снимаемые сидушки -_-
     self:SetPackedBool("Headlights1",Panel.Headlights1 > 0)
     self:SetPackedBool("Headlights2",Panel.Headlights2 > 0)
     self:SetPackedBool("RedLights",Panel.RedLight2 > 0)
