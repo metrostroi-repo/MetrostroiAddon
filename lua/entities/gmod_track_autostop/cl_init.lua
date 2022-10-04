d6b52109f5bc6830b3879a1280725447ef6efbee
TheFUlDeep <zlobindd@mail.ru>
2022-10-04 21:42:28 +0300

    animate func
    Метод Animate теперь берется из ентити сигнала (шобы повторяемость кода вот это вот все да)

--- lua/entities/gmod_track_autostop/cl_init.lua
+++ lua/entities/gmod_track_autostop/cl_init.lua
@@ -5,49 +5,9 @@ function ENT:Initialize()
     self.Anims = {}
 end
 
-function ENT:Animate(clientProp, value, min, max, speed, damping, stickyness)
-    local id = clientProp
-    if not self.Anims[id] then
-        self.Anims[id] = {}
-        self.Anims[id].val = value
-        self.Anims[id].V = 0.0
-    end
-
-    if damping == false then
-        local dX = speed * self.DeltaTime
-        if value > self.Anims[id].val then
-            self.Anims[id].val = self.Anims[id].val + dX
-        end
-        if value < self.Anims[id].val then
-            self.Anims[id].val = self.Anims[id].val - dX
-        end
-        if math.abs(value - self.Anims[id].val) < dX then
-            self.Anims[id].val = value
-        end
-    else
-        -- Prepare speed limiting
-        local delta = math.abs(value - self.Anims[id].val)
-        local max_speed = 1.5*delta / self.DeltaTime
-        local max_accel = 0.5 / self.DeltaTime
-
-        -- Simulate
-        local dX2dT = (speed or 128)*(value - self.Anims[id].val) - self.Anims[id].V * (damping or 8.0)
-        if dX2dT >  max_accel then dX2dT =  max_accel end
-        if dX2dT < -max_accel then dX2dT = -max_accel end
-
-        self.Anims[id].V = self.Anims[id].V + dX2dT * self.DeltaTime
-        if self.Anims[id].V >  max_speed then self.Anims[id].V =  max_speed end
-        if self.Anims[id].V < -max_speed then self.Anims[id].V = -max_speed end
-
-        self.Anims[id].val = math.max(0,math.min(1,self.Anims[id].val + self.Anims[id].V * self.DeltaTime))
-
-        -- Check if value got stuck
-        if (math.abs(dX2dT) < 0.001) and stickyness and (self.DeltaTime > 0) then
-            self.Anims[id].stuck = true
-        end
-    end
-    return min + (max-min)*self.Anims[id].val
-end
+hook.Add("InitPostEntity","metrostroi autostop entity get animate signal",function()
+	scripted_ents.GetStored("gmod_track_autostop").t.Animate = scripted_ents.GetStored("gmod_track_signal").t.Animate
+end)
 
 function ENT:Think()
     self:SetNextClientThink(CurTime() + 0.0333)
