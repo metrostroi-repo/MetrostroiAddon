63780318e4f1058b5cee75c0e4abfdf92752aa3f
Moon Horse <akaikangaroo@gmail.com>
2025-12-14 20:32:20 +0300

    screenhide parameter for DVR-87 added

--- lua/entities/gmod_subway_81-717_lvz/cl_init.lua
+++ lua/entities/gmod_subway_81-717_lvz/cl_init.lua
@@ -2445,6 +2445,7 @@ ENT.ButtonMap["DVR_87"] = {
     height = 80,
     scale = 0.0625,
     hide=0.8,
+    screenHide = true,
 
     buttons = {
         {ID = "DVRDisconnectToggle", x=0, y=0, w=187, h=80, tooltip="",var="DVRDisconnect",states={"Train.Buttons.Opened","Train.Buttons.Closed"}},   
