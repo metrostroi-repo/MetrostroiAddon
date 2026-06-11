6b23e7a40b6e05737a7d70864d305c0b8b3550d4
kosmik641 <minecraftgame2002@gmail.com>
2026-06-11 04:20:28 +0300

    Добавлена поддержка кастомных звуков проезда стрелки в gmod_subway_wheels.

--- lua/entities/gmod_train_wheels/init.lua
+++ lua/entities/gmod_train_wheels/init.lua
@@ -42,7 +42,7 @@ function ENT:PhysicsCollide(data,physobj)
 
 				local pitch_var = math.random(90,110)
 				local pitch = pitch_var*math.max(0.8,math.min(1.3,speed/40))
-				self:EmitSound("subway_trains/bogey/junct_"..math.random(1,4)..".wav",100,pitch )
+				self:EmitSound(self.JunctionSounds[math.random(#self.JunctionSounds)],100,pitch )
 			end
 		end
 	end
--- lua/entities/gmod_train_wheels/shared.lua
+++ lua/entities/gmod_train_wheels/shared.lua
@@ -8,3 +8,10 @@ ENT.Instructions    = ""
 
 ENT.Spawnable       = false
 ENT.AdminSpawnable  = false
+
+ENT.JunctionSounds = {
+    "subway_trains/bogey/junct_1.wav",
+    "subway_trains/bogey/junct_2.wav",
+    "subway_trains/bogey/junct_3.wav",
+    "subway_trains/bogey/junct_4.wav"
+}
